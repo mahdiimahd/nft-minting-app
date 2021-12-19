40f6cfdd7e08b73bb1416d81d13afc1a03ae3046
Mahdi <abosi@umich.edu>
2021-12-19 13:54:57 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -207,7 +207,7 @@ function App() {
         <s.SpacerSmall />
         <ResponsiveWrapper flex={1} style={{ padding: 24 }} test>
           <s.Container flex={1} jc={"center"} ai={"center"}>
-            <StyledImg alt={"example"} src={"/config/images/example.gif"} />
+//             <StyledImg alt={"example"} src={"/config/images/example.gif"} />
           </s.Container>
           <s.SpacerLarge />
           <s.Container
@@ -399,11 +399,11 @@ function App() {
           </s.Container>
           <s.SpacerLarge />
           <s.Container flex={1} jc={"center"} ai={"center"}>
-            <StyledImg
-              alt={"example"}
-              src={"/config/images/example.gif"}
-              style={{ transform: "scaleX(-1)" }}
-            />
+//             <StyledImg
+//               alt={"example"}
+//               src={"/config/images/example.gif"}
+//               style={{ transform: "scaleX(-1)" }}
+//             />
           </s.Container>
         </ResponsiveWrapper>
         <s.SpacerMedium />
